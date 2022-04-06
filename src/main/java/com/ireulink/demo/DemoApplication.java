c78b9275373caf65d4d20f5bf0149324062a4da9
hamid <hamid.shafikani@gmail.com>
2022-04-07 00:22:03 +0200

    Update owasp

--- src/main/java/com/ireulink/demo/DemoApplication.java
+++ src/main/java/com/ireulink/demo/DemoApplication.java
@@ -1,11 +1,11 @@
 package com.ireulink.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableFeignClients
 public class DemoApplication {
--- src/main/java/com/ireulink/demo/service/impl/EvStatisticServiceImpl.java
+++ src/main/java/com/ireulink/demo/service/impl/EvStatisticServiceImpl.java
@@ -2,16 +2,12 @@ package com.ireulink.demo.service.impl;
 
 import com.ireulink.demo.converter.EvStaticMapper;
 import com.ireulink.demo.dao.EvStatisticsDao;
-import com.ireulink.demo.dto.EvInfoTo;
 import com.ireulink.demo.dto.EvStatisticsTo;
-import com.ireulink.demo.model.EvInfo;
 import com.ireulink.demo.model.EvStatistics;
 import com.ireulink.demo.service.EvStatisticsService;
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
 
