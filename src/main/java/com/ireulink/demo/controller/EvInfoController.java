ba38cde500a04dcdfb634dd39cff5aaab77977ad
hamid <hamid.shafikani@gmail.com>
2022-04-17 01:47:16 +0200

    Add build info

--- src/main/java/com/ireulink/demo/controller/EvInfoController.java
+++ src/main/java/com/ireulink/demo/controller/EvInfoController.java
@@ -20,7 +20,7 @@ public class EvInfoController {
         return evInfosService.getEvInfos();
     }
 
-    @GetMapping("/test")
+    @GetMapping("/testv2")
     public List<EvInfoTo> getEvInfosTes() {
         return evInfosService.getEvInfos();
     }
